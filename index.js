9efa71cf426ea419917fb30ee6b6747be34c230f
sato-s <s.sato.desu@gmail.com>
2024-08-29 22:47:06 +0900

    format

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-$( document ).ready(function() {
- draw();
+$(document).ready(function () {
+  draw();
 })
 
-var draw = function(){
+var draw = function () {
   c3.generate({
     data: {
       x: 'x',
       columns: [
-        ['x', ].concat(data.map(function(datum){ return datum.time})),
-        ['体重', ].concat(data.map(function(datum){ return datum.weight})),
+        ['x',].concat(data.map(function (datum) {return datum.time})),
+        ['体重',].concat(data.map(function (datum) {return datum.weight})),
         // ['BMI', ].concat(data.map(function(datum){ return datum.weight / (1.68 ** 2)})),
       ],
       // axes: {
@@ -21,18 +21,18 @@ var draw = function(){
         lines: [
           {value: 62.1, text: 'BMI:22'},
           {value: 70.6, text: 'BMI:25'},
-         ]
+        ]
       },
     },
     axis: {
       x: {
-          type: 'timeseries',
-          tick: {
-              format: '%Y-%m-%d'
-          },
+        type: 'timeseries',
+        tick: {
+          format: '%Y-%m-%d'
+        },
       },
       y: {
-          min: 61
+        min: 61
       }
       // y2: {
       //   show: true
